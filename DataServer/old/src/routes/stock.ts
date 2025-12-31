aa1f339c98c83cc319e1f302d146d2a59ae8f8e9
tabxx <2969117392@qq.com>
2026-01-01 02:35:58 +0800

    refactor(DataServer): Removed Old Items.

--- DataServer/old/src/routes/stock.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as controller from '../controllers/stockController';
-
-const router = Router();
-
-router.get('/', controller.list);
-router.get('/:symbol', controller.getBySymbol);
-
-export default router;
